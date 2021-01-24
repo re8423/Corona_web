a9220d056ea9785a6d1e4ec051392f9c8a0bccfc
Reiii <50512871+re8423@users.noreply.github.com>
2021-01-24 19:23:08 +0900

    Finishing touches

---
+++ Corona_Web/Readme.md
@@ -0,0 +1,15 @@
+This webpage displays various data and information about the effects of the Covid pandemic on the top 10 worst-hit countries in the world, as well as evaluate the risk factor of all worldwide countries based on the number of confirmed infected cases. 
+
+The html file index.html is the home/starting page of the website.
+
+The 10 folders for countries Brazil~US contains a csv file used for each of the bar graphs in main.html. Worldconf, Worlddeaths, Worldhosp.csv contains data used for the world cloropeth map in index.html.
+
+Due to unavailaibilty of data the data used for Effect on economy, Effect on mental health, and hospital availiablity was generated randomly. 
+
+All refrences on data documented in about.html as well as the JSDoc documentation.
+
+All D3 graphs and charts refrenced in JSDoc documentation.
+
+Bootstrap v3 was used to decorate the buttons and various inputs.
+
+No additional installations or specific input is required to run the site. 
--- Corona_Web/data.js
+++ Corona_Web/data.js
@@ -1,7 +1,11 @@
+/** Due to the unavaialability of data all data for the Hospital availability, Effect on economy, and Effect on mental health were all generated randomly with python.
+ */
+
 data = [{"Country":"US","Deaths":395851,"TotalCases":23758855},{"Country":"Brazil","Deaths":209296,"TotalCases":8455059},{"Country":"India","Deaths":152274,"TotalCases":10557985},{"Country":"Mexico","Deaths":140241,"TotalCases":1630258},{"Country":"UK","Deaths":88590,"TotalCases":3357361},{"Country":"Italy","Deaths":81325,"TotalCases":2368733},{"Country":"France","Deaths":82225,"TotalCases":2268733},{"Country":"Russia","Deaths":64134,"TotalCases":3507201},{"Country":"Iran","Deaths":56717,"TotalCases":1324395},{"Country":"Spain","Deaths":53314,"TotalCases":2252164}]
 
 links = [{"Country":"US", "Link":"https://www.usa.gov/coronavirus"},{"Country":"Brazil","Link":"https://home.kpmg/xx/en/home/insights/2020/04/brazil-government-and-institution-measures-in-response-to-covid.html"},{"Country":"India","Link":"https://home.kpmg/xx/en/home/insights/2020/04/india-government-and-institution-measures-in-response-to-covid.html"},{"Country":"Mexico","Link":"https://home.kpmg/xx/en/home/insights/2020/04/mexico-government-and-institution-measures-in-response-to-covid.html"},{"Country":"UK","Link":"https://www.gov.uk/guidance/national-lockdown-stay-at-home"},{"Country":"Italy","Link":"http://www.salute.gov.it/portale/nuovocoronavirus/dettaglioFaqNuovoCoronavirus.jsp?lingua=english&id=230"},{"Country":"France","Link":"https://www.gouvernement.fr/en/coronavirus-covid-19"},{"Country":"Russia","Link":"https://home.kpmg/xx/en/home/insights/2020/04/russia-government-and-institution-measures-in-response-to-covid.html"},{"Country":"Iran","Link":"https://home.kpmg/xx/en/home/insights/2020/04/iran-government-and-institution-measures-in-response-to-covid.html"},{"Country":"Spain","Link":"https://www.esmadrid.com/en/information-coronavirus?utm_referrer=https%3A%2F%2Fwww.google.com%2F"}]
 
+/** This function takes the link to the gov info website for the appropriate country from the links array */
 function guidelines(){
     var selected = document.getElementById("sel1");
     var c = selected.value;
@@ -23,8 +27,8 @@ function guidelines(){
     }
     return link
 }
-
-function getc(){
+/** This function initializes everything on the main.html. The first for loop generates the options in the select input using the above json array*/
+function init(){
 
     c = ""
 
@@ -72,7 +76,9 @@ function getc(){
     
 }
 
-function subd(){
+/** This function updates all the info shown on main.html according to the country selected in the select input */
+
+function upda(){
 
     var selected = document.getElementById("sel1");
     var c = selected.value;
@@ -111,6 +117,8 @@ function subd(){
     document.getElementById("totalcases").innerHTML = tc;
     
 }
+
+/** This is the hospchart class used to create the horizontal barchart on main.html. The d3 code for the barchart was taken from CKelleher."Horizontal Bar Chart." Bl.ocks. http://bl.ocks.org/curran/8ac62d283a7aa6e95de8 - Released under the MIT licence*/
 
 class hospchart{
     constructor(name){
@@ -188,6 +196,10 @@ class hospchart{
     }
 }
 
+/** This function takes the type of data wanted and the country from the select option and returns a path to the appropriate csv file:
+ * e.g. type -> 'deaths' and selected -> 'Spain' returns Spain/Spaindeaths.csv to be used for creating the barchart objects
+ */
+
 function getnametrue(type){
     var selected = document.getElementById("sel1");
     var c = selected.value;
@@ -208,6 +220,9 @@ function getnametrue(type){
     fin = fin.concat('.csv')
     return fin
 }
+
+
+/** This is the barchart class used to create the various barcharts on main.html. The d3 code for the barchart was taken from: d3noob."Simple d3.js bar chart." Bl.ocks. https://bl.ocks.org/d3noob/8952219 - Released under the MIT licence */
 
 class barchar {
     constructor(name, total, type) {
@@ -324,3 +339,96 @@ class barchar {
 
 }
 
+/** This is the worldmap class used to create the worldmap on index.html. The d3 code for the barchart was taken from: BWelsh. “World Choropleth.” Bl.ocks. http://bl.ocks.org/palewire/d2906de347a160f38bc0b7ca57721328 - Released under the MIT licence */
+
+class worldmap {
+    constructor(type){
+        this.type = type
+
+        if(type == 'deaths'){
+            this.text = "Total deaths per million"
+            this.link = "worlddeaths.csv"
+            this.domain = [1, 201, 401, 601, 801, 1000]
+            this.labels = ['No Data', '1-200', '201-400', '401-600', '601-800', '801-1000', '> 1000']
+            this.colour = d3.schemeReds[7]
+        } else if (type =='conf'){
+            this.text = "Total cases per million"
+            this.link = "worldconf.csv"
+            this.domain = [1, 10001, 15001, 30001, 50001, 70000]
+            this.labels = ['No Data', '1-10000', '10001-15000: LOW RISK AREA', '15001-30000', '30001-50000: MEDIUM RISK AREA', '50001-70000', '> 70000: HIGH RISK AREA']
+            this.colour = d3.schemeBlues[7]
+
+        } else if (type =='hosp'){
+            this.text = "Total Hospital beds available per thousand"
+            this.link = "worldhosp.csv"
+            this.domain = [0, 1.1, 2.1, 3.1, 4.1, 5.1]
+            this.labels = ['No Data', '0-1', '1.1-2', '2.1-3', '3.1-4', '4.1-5', '> 5']
+            this.colour = d3.schemeGreens[7]
+        }
+
+        this.width = 960
+        this.height = 500
+
+        // The svg
+        var svg = d3.select('#map').append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height)
+        // Map and projection
+        var path = d3.geoPath();
+        var projection = d3.geoNaturalEarth()
+        .scale(this.width / 2 / Math.PI)
+        .translate([this.width / 2, this.height / 2])
+        var path = d3.geoPath()
+        .projection(projection);
+
+        // Data and color scale
+        var data = d3.map();
+        var colorScheme = this.colour;
+        var colorScale = d3.scaleThreshold()
+        .domain(this.domain)
+        .range(colorScheme);
+
+        // Legend
+        var g = svg.append("g")
+            .attr("class", "legendThreshold")
+            .attr("transform", "translate(0,247)");
+        g.append("text")
+            .attr("class", "caption")
+            .attr("x", 0)
+            .attr("y", -6)
+            .text(this.text);
+            var labels = this.labels;
+            var legend = d3.legendColor()
+            .labels(function (d) { return labels[d.i]; })
+            .shapePadding(4)
+            .scale(colorScale);
+            svg.select(".legendThreshold")
+            .call(legend);
+
+
+        // Load external data and boot
+        d3.queue()
+        .defer(d3.json, "http://enjalot.github.io/wwsd/data/world/world-110m.geojson")
+        .defer(d3.csv, this.link, function(d) { data.set(d.code, +d.total); })
+        .await(ready);
+
+        function ready(error, topo) {
+        if (error) throw error;
+
+        // Draw the map
+        svg.append("g")
+            .attr("class", "countries")
+            .selectAll("path")
+            .data(topo.features)
+            .enter().append("path")
+                .attr("fill", function (d){
+                    // Pull data for this country
+                    d.total = data.get(d.id) || 0;
+                    // Set the color
+                    return colorScale(d.total);
+                })
+                .attr("d", path);
+        }
+
+    }
+}
